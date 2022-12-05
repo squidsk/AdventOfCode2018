9d4888948c3689bda307912a79979515f623f0f8
squidsk <squidsk@gmail.com>
2022-12-05 11:22:52 -0700

    Day 13 Part 1 & Part 2

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day13 {
     
@@ -22,7 +23,9 @@ namespace Day13 {
         internal int x;
         internal int y;
         internal char symbol;
+        internal char lastTrack;
         TurnDirection nextTurn;
+        internal bool hasCrashed;
         
         
         public Point(char symbol, int x, int y) {
@@ -30,60 +33,113 @@ namespace Day13 {
             this.x = x;
             this.y = y;
             this.symbol = symbol;
+            if(symbol == 'v' || symbol == '^') lastTrack = '|';
+            else lastTrack = '-';
             nextTurn = TurnDirection.Left;
+            hasCrashed = false;
         }
         
-        public void move(char nextSymbol) {
+        public void move(char nextTrack) {
             if(symbol == '^') {
                 x = x - 1;
-                if(nextSymbol == '\'') symbol = '<';
-                else if(nextSymbol == '/') symbol = '>';
+                if(nextTrack == '\\') symbol = '<';
+                else if(nextTrack == '/') symbol = '>';
+                else if(nextTrack == '+') {
+                    if(nextTurn == TurnDirection.Left) {
+                        nextTurn = TurnDirection.Straight;
+                        symbol = '<';
+                    } else if (nextTurn == TurnDirection.Right) {
+                        nextTurn = TurnDirection.Left;
+                        symbol = '>';
+                    } else {
+                        nextTurn = TurnDirection.Right;
+                    }
+                }
             } else if (symbol == 'v') {
                 x = x + 1;
-                if(nextSymbol == '\'') symbol = '>';
-                else if(nextSymbol == '/') symbol = '<';
+                if(nextTrack == '\\') symbol = '>';
+                else if(nextTrack == '/') symbol = '<';
+                else if(nextTrack == '+') {
+                    if(nextTurn == TurnDirection.Left) {
+                        nextTurn = TurnDirection.Straight;
+                        symbol = '>';
+                    } else if (nextTurn == TurnDirection.Right) {
+                        nextTurn = TurnDirection.Left;
+                        symbol = '<';
+                    } else {
+                        nextTurn = TurnDirection.Right;
+                    }
+                }
             } else if(symbol == '<') {
                 y = y - 1;
-                if(nextSymbol == '\'') symbol = '^';
-                else if(nextSymbol == '/') symbol = 'v';
+                if(nextTrack == '\\') symbol = '^';
+                else if(nextTrack == '/') symbol = 'v';
+                else if(nextTrack == '+') {
+                    if(nextTurn == TurnDirection.Left) {
+                        nextTurn = TurnDirection.Straight;
+                        symbol = 'v';
+                    } else if (nextTurn == TurnDirection.Right) {
+                        nextTurn = TurnDirection.Left;
+                        symbol = '^';
+                    } else {
+                        nextTurn = TurnDirection.Right;
+                    }
+                }
             } else { //if(symbol == '>') {
                 y = y + 1;
-                if(nextSymbol == '\'') symbol = 'v';
-                else if(nextSymbol == '/') symbol = '^';
+                if(nextTrack == '\\') symbol = 'v';
+                else if(nextTrack == '/') symbol = '^';
+                else if(nextTrack == '+') {
+                    if(nextTurn == TurnDirection.Left) {
+                        nextTurn = TurnDirection.Straight;
+                        symbol = '^';
+                    } else if (nextTurn == TurnDirection.Right) {
+                        nextTurn = TurnDirection.Left;
+                        symbol = 'v';
+                    } else {
+                        nextTurn = TurnDirection.Right;
+                    }
+                }
             }
+            lastTrack = nextTrack;
         }
         
         public int distanceTo(Point p) {
             return Math.Abs(x - p.x) + Math.Abs(y - p.y);
         }
         
-        public char getSymbol() {
-            return symbol;
-        }
-        
-        public int getX() { return x; }
-        public int getY() { return y; }
     }
     
     class PointComparer : IComparer<Point> {
-        public int Compare(Point x, Point y) {
-            if(x == default(Point) && y == default(Point)) return 0;
-            if(x == default(Point)) return -1;
-            if(y == default(Point)) return 1;
-            if(x.getX() == y.getX() && x.getY() == y.getY()) return 0;
-            if(x.getX() < y.getX() || (x.getX() == y.getX() && x.getY() < y.getY())) return -1;
+        public int Compare(Point p1, Point p2) {
+            if(p1 == default(Point) && p2 == default(Point)) return 0;
+            if(p1 == default(Point)) return -1;
+            if(p2 == default(Point)) return 1;
+            if(p1.x == p2.x && p1.y == p2.y) return 0;
+            if(p1.x < p2.x || (p1.x == p2.x && p1.y < p2.y)) return -1;
             return 1;
         }
     }
 
     class Program
     {
+        static int maxWidth;
+        
         public static void Main(string[] args)
         {
+            //Part1("test.txt");
+            //Part1("input.txt");
+            Part2("test4.txt");
+            Part2("input.txt");
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+
+        static void Part1(string filename)
+        {
             SortedSet<Point> sortedPoints = new SortedSet<Point>(new PointComparer());
-            HashSet<Point> points = new HashSet<Point>();
-            var lines = File.ReadAllLines("test.txt");
-            int numTurns = 0;
+            var lines = File.ReadAllLines(filename);
             
             char[,] display = new char[lines.Length, lines[0].Length];
             
@@ -91,43 +147,147 @@ namespace Day13 {
                 for(int j=0; j < lines[0].Length; j += 1) {
                     display[i,j] = lines[i][j];
                     if(isTrain(display[i,j])) {
-                        points.Add(new Point(display[i,j], i, j));
+                        sortedPoints.Add(new Point(display[i,j], i, j));
                     }
                 }
             }
             displayTracks(display);
             int turns = 1;
-            foreach(Point p in points) {
-                char nextChar = getNextSymbol(display, p);
-                int currentX = p.getX();
-                int currentY = p.getY();
-                char replacementChar = getReplacementTrack(currentX, currentY);
-                p.move(nextChar);
-                if(isTrain(nextChar)) {
-                    Console.WriteLine("Train crash at location X:{0}, Y:{1}", p.getX(),p.getY());
-                } else {
-                    
+            bool crash = false;
+            
+            while(!crash){
+                SortedSet<Point> tempPoints = new SortedSet<Point>(new PointComparer());
+                foreach(Point p in sortedPoints) {
+                    char nextChar = getNextSymbol(display, p);
+                    int currentX = p.x;
+                    int currentY = p.y;
+                    char lastTrack = p.lastTrack;
+                    p.move(nextChar);
+                    display[currentX, currentY] = lastTrack;
+                    if(isTrain(nextChar)) {
+                        display[p.x,p.y] = 'X';
+                        displayTracks(display);
+                        Console.WriteLine("On turn {2}, trains crashed at location X:{0}, Y:{1}", p.x,p.y, turns);
+                        crash = true;
+                    } else {
+                        display[p.x,p.y] = p.symbol;
+                    }
+                    //displayTracks(display);
+                    if(crash) break;
+                    tempPoints.Add(p);
+                }
+                sortedPoints = tempPoints;
+                turns += 1;
+            }
+        }
+
+        static void Part2(string filename)
+        {
+            SortedSet<Point> sortedPoints = new SortedSet<Point>(new PointComparer());
+            var lines = File.ReadAllLines(filename);
+            
+            char[,] display = new char[lines.Length, lines[0].Length];
+            maxWidth = lines[0].Length;
+            
+            for(int i = 0; i<lines.Length; i +=1) {
+                for(int j=0; j < lines[0].Length; j += 1) {
+                    display[i,j] = lines[i][j];
+                    if(isTrain(display[i,j])) {
+                        sortedPoints.Add(new Point(display[i,j], i, j));
+                    }
                 }
             }
+            displayTracks(display);
+            int turns = 1;
+            bool crash = false;
             
-            
-            Console.Write("Press any key to continue . . . ");
-            Console.ReadKey(true);
+            while(sortedPoints.Count > 1){
+                SortedSet<Point> tempPoints = new SortedSet<Point>(new PointComparer());
+                HashSet<Point> crashPoints = new HashSet<Point>();
+                foreach(Point p in sortedPoints) {
+                    crash = false;
+                    if(!crashPoints.Any(m => m.x == p.x && m.y == p.y)) {
+                        char nextChar = getNextSymbol(display, p);
+                        int currentX = p.x;
+                        int currentY = p.y;
+                        char lastTrack = p.lastTrack;
+                        p.move(nextChar);
+                        display[currentX, currentY] = lastTrack;
+                        if(isTrain(nextChar)) {
+                            display[p.x,p.y] = sortedPoints.First(m => m != p && m.x == p.x && m.y == p.y).lastTrack; //getReplacementTrack(p, display);
+                            //Console.WriteLine("On turn {2}, trains crashed at location X:{0}, Y:{1}", p.x,p.y, turns);
+                            crash = true;
+                        } else {
+                            display[p.x,p.y] = p.symbol;
+                        }
+                        //displayTracks(display);
+                        if(crash){
+                            crashPoints.Add(p);
+                            tempPoints.RemoveWhere(m => m.x == p.x && m.y == p.y);
+                        } else {
+                            tempPoints.Add(p);
+                        }
+                    } else {
+                        crashPoints.RemoveWhere(m => m.x == p.x && m.y == p.y);
+                    }
+                }
+                //Console.WriteLine("End of Turn {0}", turns);
+                //displayTracks(display);
+                sortedPoints = tempPoints;
+                turns += 1;
+            }
+            displayTracks(display);
+            Console.WriteLine("On turn {2}, there is one train left at location X:{0}, Y:{1}", sortedPoints.First().x,sortedPoints.First().y, turns);
         }
 
-        static char getReplacementTrack(int x, int y)
-        {
-            if(isTrackAbove(x, y) && isTrackBelow(x,y) && isTrackLeft(x,y) && isTrackRight(x,y)) return '+';
-            
+        static bool isTrackLeft(Point p, char[,] display) {
+            return p.y > 0 && isTrack(display[p.x, p.y-1]);
         }
-
+        
+        static bool isTrackRight(Point p, char[,] display) {
+            return p.y < maxWidth -1 && isTrack(display[p.x, p.y+1]);
+        }
+        
+        static bool isTrackAbove(Point p, char[,] display) {
+            return p.x > 0 && isTrack(display[p.x-1, p.y]);
+        }
+        
+        static char getReplacementTrack(Point p, char[,] display) {
+            char track;
+            if(isTrackLeft(p, display)) {
+                if(isTrackAbove(p,display)) {
+                    if(isTrackRight(p,display)) {
+                        track = '+';
+                    } else {
+                        track = '/';
+                    }
+                } else if(isTrackRight(p, display)) {
+                    track = '-';
+                } else {
+                    track = '\\';
+                }
+            } else {
+                if(isTrackAbove(p,display)) {
+                    if(isTrackRight(p,display)) {
+                        track = '\\';
+                    } else {
+                        track = '|';
+                    }
+                } else {
+                    track = '/';
+                }
+            }
+            return track;
+        }
+        
+        
         static char getNextSymbol(char[,] display, Point p)
         {
-            switch(p.getSymbol()) {
-                    case 'v': return display[p.getX() + 1, p.getY()];
-                    case '^': return display[p.getX() - 1, p.getY()];
-                    case '<': return display[p.getX(), p.getY() - 1];
-                    case '>': return display[p.getX(), p.getY() + 1];
+            switch(p.symbol) {
+                    case 'v': return display[p.x + 1, p.y];
+                    case '^': return display[p.x - 1, p.y];
+                    case '<': return display[p.x, p.y - 1];
+                    case '>': return display[p.x, p.y + 1];
                     default: return '*';
             }
         }
@@ -139,10 +299,15 @@ namespace Day13 {
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
         
         static bool isTrain(char ch) {
             return ch == '^' || ch == 'v' || ch == '>' || ch == '<';
+        }
+        
+        static bool isTrack(char ch) {
+            return ch == '|' || ch == '-' || ch == '+' || ch == '\\' || ch == '/';
         }
     }
 }
